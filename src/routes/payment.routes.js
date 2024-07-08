595221cd6746e44d65de21cc1a3f879dbba35a53
Sahil Kumar <sahilkumar142002@gmail.com>
2024-07-08 18:12:31 +0530

    "Updated middleware import and usage in payment routes"

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { makePayment, purchasedCourse } = require('../controllers/payment.controller')
-const { limiter } = require('../middleware/access.limiter');
+const { apiLimiter } = require('../middleware/access.limiter');
 
-router.route('/make-payment').get(limiter, makePayment);
+router.route('/make-payment').get(apiLimiter, makePayment);
 
 router.route('/payment-status').post(purchasedCourse);
 
